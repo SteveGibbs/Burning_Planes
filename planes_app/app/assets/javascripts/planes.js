d1758a7ccd7ce2ffb403d89b90fc25a73f1a25ce
Peter-Fagan <peter_fagan@hotmail.com>
2016-09-14 14:27:46 +1000

    Create planes generates seating diagram

--- planes_app/app/assets/javascripts/planes.js
+++ planes_app/app/assets/javascripts/planes.js
@@ -1,24 +1,31 @@
-var rows = 4;
-var columns = 6;
-var characters = "ABCD";
+// var rows = 4;
+// var columns = 10;
+var characters = "ABCDEFGHIJKL";
+// var rowChar = characters.slice(0, rows);
 var gridNo;
-var squares = rows * columns;
 var gridLocation;
 
+var planesize = function() {
+  var columnWidth = columns * 30;
+  $(".board").css("width", columnWidth + "px");
+  console.log(columnWidth);
+};
+
 var createGrid = function() {
-  for (char of characters) {
-  var letter = char;
-    for (var i = 0; i<columns; i++) {
-      gridNo = i+1;
-      gridLocation = letter+gridNo;
-      console.log(letter, gridNo);
-      var $tile = $('<div class=' + letter + ' id =' + gridNo + '></div>');
-      $(".board").append($tile);
-      $tile.addClass("seat");
+  var rowChar = characters.slice(0, rows);
+    for (char of rowChar) {
+    var letter = char;
+      for (var i = 0; i<columns; i++) {
+        gridNo = i+1;
+        gridLocation = letter+gridNo;
+        var $tile = $('<div class=' + letter + ' id =' + gridNo + '></div>');
+        $(".board").append($tile);
+        $tile.addClass("seat");
+      }
     }
-  }
+    planesize();
 };
 
 $(document).ready(function(){
   createGrid();
-})
+});
