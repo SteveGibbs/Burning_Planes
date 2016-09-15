f71cbe84619428e99f3b81e0352f9933f3944b45
SteveGibbs <gibbsstephen@hotmail.com>
2016-09-16 01:01:39 +1000

    burning planes project

--- planes_app/db/seeds.rb
+++ planes_app/db/seeds.rb
@@ -11,16 +11,22 @@ u1 = User.create :firstname => "John", :lastname => "Doe", :email => "johndoe@gm
 
 u2 = User.create :firstname => "Jane", :lastname => "Doe", :email => "janedoe@gmail.com", :admin => "false", :password => "chicken", :password_confirmation => "chicken"
 
+u3 = User.create :firstname => "Judy", :lastname => "Doe", :email => "judydoe@gmail.com", :admin => "false", :password => "chicken", :password_confirmation => "chicken"
+
+
 p u1
 p u2
+p u3
 
 Plane.destroy_all
 
 p1 = Plane.create :name => "747", :row => "3", :column => "3"
 p2 = Plane.create :name => "737", :row => "4", :column => "4"
+p3 = Plane.create :name => "727", :row => "2", :column => "5"
 
 p p1
 p p2
+p p3
 
 Flight.destroy_all
 
@@ -28,9 +34,17 @@ f1 = Flight.create :flightref => "QF27", :date => "30 Jan 2000", :to => "MEL", :
 
 f2 = Flight.create :flightref => "QF20", :date => "31 Jan 2000", :to => "HOB", :from => "SYD"
 
+f3 = Flight.create :flightref => "QF25", :date => "31 Jan 2000", :to => "MEL", :from => "SYD"
+
+
 p f1
 p f2
-
+p f3
 
 p1.flights << f1
 p2.flights << f2
+p1.flights << f3
+
+r1 = Reservation.create :flight_id => "1", :user_id => "1", seat_num "B2"
+
+# f1.users << u1
