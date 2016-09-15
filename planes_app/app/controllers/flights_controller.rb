d9600c6fc03a88a7f6c1967197952e8ac880d775
SteveGibbs <gibbsstephen@hotmail.com>
2016-09-15 10:22:34 +1000

    fixed up plane_id

--- planes_app/app/controllers/flights_controller.rb
+++ planes_app/app/controllers/flights_controller.rb
@@ -69,6 +69,6 @@ class FlightsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def flight_params
-      params.require(:flight).permit(:flightref, :date, :to, :from, :plane)
+      params.require(:flight).permit(:flightref, :date, :to, :from, :plane_id)
     end
 end
